f14f9b847fdb5b8bade70c5753ba8b3c2c0c560e
juliaverdickt <93015589+juliaverdickt@users.noreply.github.com>
2023-11-25 12:33:36 -0800

    Julia's work

--- 201_probs.py
+++ 201_probs.py
@@ -4,63 +4,71 @@ def number_permutations(numbers):
     return [p for p in permutations(numbers)]
 
 example_numbers = [10,11,12,13,14]
-permutations_result = number_permutations(example_numbers)
 
+permutations_result = number_permutations(example_numbers)
 
 fixed_perm = (14,13,12,11,10)
 
 
-comparison_results  = []
-for i in range(len(permutations_result)):
-    temp_list = []
-    for j in range(5):
-        if permutations_result[i][j] < fixed_perm[j]:
-            temp_list.append("L")
-        elif permutations_result[i][j] > fixed_perm[j]:
-            temp_list.append("W")
-        else:
-            temp_list.append("T")
-    comparison_results.append(temp_list)
-    
-    
-p1_wins = []
-        
-for perm in comparison_results:
-    if perm.count("W")>=3:
-        p1_wins.append(1)
-    else:
-        p1_wins.append(0)
-        
-cond_probp1win = sum(p1_wins)/len(permutations_result)
-cond_probp1win 
-
-
-
-permutations_resultp2 = number_permutations(example_numbers)
-
-comparison_results2  = []
-
-for k in range(len(permutations_resultp2)):
-    for i in range(len(permutations_result)):
+def fixed_comparison_func(perms, fixed_perm):
+    comparison_results  = []
+    for i in range(len(perms)):
         temp_list = []
         for j in range(5):
-            if permutations_result[i][j] < permutations_resultp2[k][j]:
+            if perms[i][j] < fixed_perm[j]:
                 temp_list.append("L")
-            elif permutations_result[i][j] > permutations_resultp2[k][j]:
+            elif perms[i][j] > fixed_perm[j]:
                 temp_list.append("W")
             else:
                 temp_list.append("T")
-        comparison_results2.append(temp_list)
-        
-        
-p1_wins_2 = []
-for perm in comparison_results2:
-    if perm.count("W")>=3:
-        p1_wins_2.append(1)
-    else:
-        p1_wins_2.append(0)
-        
-uncond_probp1win = sum(p1_wins_2)/(len(permutations_resultp2)* len(permutations_result))
-uncond_probp1win 
+        comparison_results.append(temp_list)
+    return(comparison_results)
+    
+comparison_fixed= fixed_comparison_func(permutations_result, fixed_perm)
+            
+def cond_prob_calculator(comparisons, cutoff, perms):
+    p1_wins = []
+    for perm in comparisons:
+        if perm.count("W")>=cutoff:
+            p1_wins.append(1)
+        else:
+            p1_wins.append(0)      
+    cond_probp1win = sum(p1_wins)/len(perms)
+    return(cond_probp1win)
 
-p1_p2_tie = []
+cond_prob_calculator(comparison_fixed, 3, permutations_result)     
+
+def unfixed_comparison_func(perms):
+    comparison_results2  = []
+    for k in range(len(perms)):
+        for i in range(len(perms)):
+            temp_list = []
+            for j in range(5):
+                if perms[i][j] < perms[k][j]:
+                    temp_list.append("L")
+                elif perms[i][j] > perms[k][j]:
+                    temp_list.append("W")
+                else:
+                    temp_list.append("T")
+            comparison_results2.append(temp_list)
+    return(comparison_results2)
+
+comparison_unfixed = unfixed_comparison_func(permutations_result)
+
+len(comparison_fixed)
+
+def uncond_prob_calculator(comparisons, cutoff, perms):
+    p1_wins_2 = []
+    for perm in comparisons:
+        if perm.count("W")>=cutoff:
+            p1_wins_2.append(1)
+        else:
+            p1_wins_2.append(0)
+    uncond_probp1win = sum(p1_wins_2)/(len(perms)**2)
+    return(uncond_probp1win) 
+
+
+p1_wins_m4 = uncond_prob_calculator(comparison_unfixed, 4, permutations_result)
+p1_wins_m4
+
+    
